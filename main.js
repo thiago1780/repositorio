2ce920f54104674d2838cb1167c1347ff480f341
thiago1780 <126567577+thiago1780@users.noreply.github.com>
2023-11-16 07:44:10 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function tocasSomPom () {
+document.querySelector('#som_tecla_pom').play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador= 0;
+
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+}
+
